f492c3583a584790429c080fcc0b2888a6faecb9
rattus-aristarchus <lankin.m.a@gmail.com>
2023-09-26 15:20:19 +0300

    bad testability

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from json import load
 
 
 def weather(city):
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    ip_address = response["ip"]
+    url = 'https://ipinfo.io/' + ip_address + '/json'
+    response = urlopen(url)
+    json = load(response)
+    city = json["city"]
+
     url = 'https://api.openweathermap.org/data/2.5/weather?q=' + \
           city + \
           '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
@@ -19,21 +26,5 @@ def weather(city):
     return msg
 
 
-def get_ip():
-    """Get user's IP"""
-    response = requests.get('https://api64.ipify.org?format=json').json()
-    return response["ip"]
-
-
-def get_city():
-    """Get user's city based on their IP"""
-
-    ip_address = get_ip()
-    url = 'https://ipinfo.io/' + ip_address + '/json'
-    response = urlopen(url)
-    json = load(response)
-    return json["city"]
-
-
 if __name__ == '__main__':
-    print(weather(get_city()))
+    print(weather())
