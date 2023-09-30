5f99da782437c6d6c864538d062efe2cb7b804f4
rattus-aristarchus <lankin.m.a@gmail.com>
2023-09-30 14:26:57 +0300

    appid as a constant

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from urllib.request import urlopen
 from json import load
 
 
+OPENWEATHERMAP_APPID = "79d1ca96933b0328e1c7e3e7a26cb347"
+
+
 def weather():
     # First, we get the IP
     response = requests.get('https://api64.ipify.org?format=json').json()
@@ -18,7 +21,8 @@ def weather():
     # Finally, hit up a weather service for weather in that city
     url = 'https://api.openweathermap.org/data/2.5/weather?q=' + \
           city + \
-          '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+          '&units=metric&lang=ru&appid=' + \
+          OPENWEATHERMAP_APPID
     weather_data = requests.get(url).json()
 
     temperature = round(weather_data['main']['temp'])
