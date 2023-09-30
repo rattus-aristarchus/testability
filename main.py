5f514be216d03bd9043edf8147f54ba7fd24b68f
rattus-aristarchus <lankin.m.a@gmail.com>
2023-09-30 14:23:43 +0300

    comments

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from json import load
 
 
 def weather():
+    # First, we get the IP
     response = requests.get('https://api64.ipify.org?format=json').json()
     ip_address = response["ip"]
+
+    # Next, determine the city based on IP
     url = 'https://ipinfo.io/' + ip_address + '/json'
     response = urlopen(url)
     json = load(response)
     city = json["city"]
 
+    # Finally, hit up a weather service for weather in that city
     url = 'https://api.openweathermap.org/data/2.5/weather?q=' + \
           city + \
           '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-
     weather_data = requests.get(url).json()
 
     temperature = round(weather_data['main']['temp'])
