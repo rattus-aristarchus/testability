2b547d685b1538448174190d27cdd59feb912a28
rattus-aristarchus <lankin.m.a@gmail.com>
2023-10-04 14:13:24 +0300

    fixed tests

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -4,15 +4,19 @@ import pytest
 import ipaddress
 
 
+CITY = ["Saint Petersburg", "St Petersburg", "Strel'na"]
+
+
 def test_weather():
     with pytest.raises(KeyError):
-        main.weather("Not a city name")
+        main.fetch_local_weather("Not a city name")
 
 
 def test_city():
     ip = "92.100.165.4"
-    assert main.fetch_city(ip) == "Saint Petersburg"
+    assert main.fetch_city(ip) in CITY
 
 
 def test_ip():
-    ipaddress.ip_address(main.fetch_ip())
+    ip = main.fetch_ip()
+    ipaddress.ip_address(ip)
