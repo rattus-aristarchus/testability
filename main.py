0cee1772b85ef1bc5276116705071c69e4063bc2
rattus-aristarchus <lankin.m.a@gmail.com>
2023-11-05 14:48:25 +0300

    added last measurements

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+import datetime
+
+import yaml
 
 import requests
 
-
 OPENWEATHERMAP_APPID = "79d1ca96933b0328e1c7e3e7a26cb347"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEASUREMENTS = os.path.join(BASE_DIR, "measurements.yml")
 
 
 def local_weather():
@@ -25,8 +30,35 @@ def local_weather():
     temperature = round(weather_data['main']['temp'])
     temperature_feels = round(weather_data['main']['feels_like'])
 
-    msg = (f"Temperature in {city}: {str(temperature)} °C\n"
-           f"Feels like {str(temperature_feels)} °C")
+    # If measurements have already been taken, compare them to current results
+    measurements = []
+    diff = None
+    diff_feels = None
+    measure_date = None
+    if os.path.exists(MEASUREMENTS):
+        measurements = yaml.safe_load(open(MEASUREMENTS, "r"))
+        last_temp = measurements[-1]["temp"]
+        last_feels = measurements[-1]["feels"]
+        diff = temperature - last_temp
+        diff_feels = temperature_feels - last_feels
+        measure_date = measurements[-1]["date"]
+
+    # Write down the current result
+    with open(MEASUREMENTS, "w") as file:
+        record = {"date": datetime.datetime.now().date(),
+                  "temp": temperature,
+                  "feels": temperature_feels}
+        measurements.append(record)
+        yaml.dump(measurements, file)
+
+    # Print the result
+    msg = (f"Temperature in {city}: {str(temperature)} °C"
+           f"\nFeels like {str(temperature_feels)} °C")
+    if (diff is not None and
+            diff_feels is not None and
+            measure_date is not None):
+        msg += (f"\nLast measurement taken on {measure_date}"
+                f"\nDifference since then - {str(diff)} (feels {str(diff_feels)})")
     print(msg)
 
 
